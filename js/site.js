50055e880f0e86b6953622815b7bf1207d563e42
RedzCafe <software@devoredev.com>
2022-09-19 18:10:09 -0400

    1st baking

--- js/site.js
+++ js/site.js
@@ -1,75 +1,82 @@
-// Section 4 Hello World
-function helloWorld(){
-    alert("hello world");
-}
-
-
 
 //get the values from the page
 //Starts or Controller Function
 function getValues(){
     //get values from the page
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
     //parse into Integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
-    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
         //we call generateNumber
-        let numbers = generateNumbers(startValue,endValue);
+        let fbArray = fizzBuzz(fizzValue,buzzValue);
         //we call displayNumbers
-        displayNumbers(numbers);
+        displayData(fbArray);
 
     }else{
         alert("You must enter an integer.")
     }
+}
 
+
+function fizzBuzz(fizzValue, buzzValue)
+{
+    let returnArray = [];
+
+    for (let i = 1; i <= 100; i++) {
+
+        if(i % fizzValue == 0 && i % buzzValue == 0){
+            returnArray.push('FizzBuzz');
+        }else if (i % fizzValue == 0){
+            returnArray.push('Fizz');
+
+        }else if (i % buzzValue == 0) {
+            returnArray.push('Buzz');
+
+        }else {
+            returnArray.push(i);
+        }  
+     }
+     return returnArray;
+}
+
+//Display or view function
+function displayData(fbArray){
+
+    let tableBody = document.getElementById("results");
+
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear the table
+    tableBody.innerHTML = "";
+
+    for (let index = 0; index < fbArray.length; index += 5) {
+        let tableRow = document.importNode(templateRow.content, true);
+
+        //grab the td, put into array
+        let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].classList.add(fbArray[index]);
+        rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index+1]);
+        rowCols[1].textContent = fbArray[index+1];
+
+        rowCols[2].classList.add(fbArray[index+2]);
+        rowCols[2].textContent = fbArray[index+2];
+
+        rowCols[3].classList.add(fbArray[index+3]);
+        rowCols[3].textContent = fbArray[index+3];
+
+        rowCols[4].classList.add(fbArray[index+4]);
+        rowCols[4].textContent = fbArray[index+4];
+
+        tableBody.appendChild(tableRow);
+        
+    }
     
-
-}
-
-//Genreate Numbers from the Start Value to the End Value
-//Logic function(s)
-function generateNumbers(sValue, eValue){
-    let numbers = [];
-
-    //we want all numbers from start to end
-    for(let index = sValue; index <= eValue; index ++){
-
-        //this will execute in a loop until index = evalue
-        numbers.push(index);
-    }
-
-
-    return numbers;
-
-
-}
-
-//Display the Even Numbers in BOLD
-//display or view function
-function displayNumbers(numbers){
-
-    let templateRows = "";
-
-    for (let index = 0; index < numbers.length; index++) {
-        
-        let className = "even";
-        
-        let number = numbers[index];
-
-        if(number % 2 == 0){
-            className = "even";
-        }
-        else{
-            className = "odd";
-        }
-        
-        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
-
-    }
-    document.getElementById("results").innerHTML = templateRows;
 
 }
